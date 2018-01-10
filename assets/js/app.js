6e5c5679387301785b82b4e321c4e6e5d499207f
mickequeen <mickequeen@gmail.com>
2018-01-10 10:53:03 -0300

    thumbnail js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,4 @@
 
-
   var spiderMan = 'http://www.omdbapi.com/?t=spider-man&plot=full&apikey=3a181f1c';
   var spiderMan2 = 'http://www.omdbapi.com/?t=spider-man-2&apikey=3a181f1c';
   var spiderMan3 = 'http://www.omdbapi.com/?t=spider-man-3&apikey=3a181f1c';
@@ -11,20 +10,37 @@
   var ironMan3 = 'http://www.omdbapi.com/?t=iron+man+3&apikey=3a181f1c';
   var Thor = 'http://www.omdbapi.com/?t=thor&apikey=3a181f1c';
 
+
 $(document).ready(function(){
 
   $ ('#login').click(function(){
     $ ('#modal').modal ();
   });
 
-
-  title(spiderMan)
-
-  function title(movie){
-    $.getJSON(movie).then(function(response){
-      console.log("título: " + response.Title);
+/*
+*thumbnail spiderMan
+*/
+    $('#firstBattle').append("<div class='col-xs-6 col-sm-6 col-md-6 col-log-6'>" +
+    "<div class='thumbnail'><img id='ultronPoster' alt='...''>" +
+      "<div class='caption'><h3 class='title' id='ultron'>" + "</h3><p>descripcion</p>" +
+      "<p><a href='#' class='btn btn-primary' role='button'>Button</a> <a href='#'' class='btn btn-default' role='button'>Button</a></p>"+
+      "</div></div></div>")
+   tituloUltron();
+    function tituloUltron (){
+    $.getJSON('http://www.omdbapi.com/?t=ultron&apikey=3a181f1c').then(function(response){
+      $('#ultron').append(response.Title);
+      });
+    }
+    ImgUltron();
+    function ImgUltron(){
+    $.getJSON('http://www.omdbapi.com/?t=ultron&apikey=3a181f1c').then(function(response){
+      $('#ultronPoster').attr('src' , response.Poster);
     });
   }
+
+
+
+
     /*$('#firstBattle').append("<div id='firstMarvel' class='col-xs-6 col-sm-6 col-ms-6 col-lg-6>" +
       "<div class=thumbnail'><img src='" + title(spiderMan)"' alt="...">")*/
 /*
@@ -35,9 +51,17 @@ $(document).ready(function(){
     var comment = $("#comment").val();
     $("#post").append("<span id='usuario'><i class='fa fa-user-circle' aria-hidden='true'></i></span>" + comment + "</br>"+"</div>" );
   });
+
 });
 
 
+
+/*
+  function image (movie){
+    $.getJSON(movie).then(function(response){
+    var poster = ("<img src='"+ response.Poster +"'>");
+  });
+  }*/
 
 
 
