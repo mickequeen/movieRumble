535bbc9f3653c786505cc4d9d1e3e0c99825b7c2
mickequeen <mickequeen@gmail.com>
2018-01-09 21:36:18 -0300

    pull a cambios de nav

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,40 @@
+
+  var spiderMan = 'http://www.omdbapi.com/?t=spider-man&plot=full&apikey=3a181f1c';
+  var spiderMan2 = 'http://www.omdbapi.com/?t=spider-man-2&apikey=3a181f1c';
+  var spiderMan3 = 'http://www.omdbapi.com/?t=spider-man-3&apikey=3a181f1c';
+  var AmazingSpiderMan = 'http://www.omdbapi.com/?t=amazing-spider-man&apikey=3a181f1c';
+  var AmazingSpiderMan2 = 'http://www.omdbapi.com/?t=amazing-spider-man-2&apikey=3a181f1c';
+  var capitanAmerica = 'http://www.omdbapi.com/?t=captain+america&apikey=3a181f1c';
+  var ironMan = 'http://www.omdbapi.com/?t=iron+man&apikey=3a181f1c';
+  var ironMan2 = 'http://www.omdbapi.com/?t=iron+man+2&apikey=3a181f1c';
+  var ironMan3 = 'http://www.omdbapi.com/?t=iron+man+3&apikey=3a181f1c';
+  var Thor = 'http://www.omdbapi.com/?t=thor&apikey=3a181f1c';
+
 $(document).ready(function(){
 
+  // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyDAkx_-jelOE2xPjZRE9dbsi6QxrVvnnsE",
+      authDomain: "movierumble-c0999.firebaseapp.com",
+      databaseURL: "https://movierumble-c0999.firebaseio.com",
+      projectId: "movierumble-c0999",
+      storageBucket: "",
+      messagingSenderId: "919196728572"
+    };
+    firebase.initializeApp(config);
 
+    title(spiderMan)
+
+  function title(movie){
+    $.getJSON(movie).then(function(response){
+      console.log("título: " + response.Title);
+    });
+  }
+    /*$('#firstBattle').append("<div id='firstMarvel' class='col-xs-6 col-sm-6 col-ms-6 col-lg-6>" +
+      "<div class=thumbnail'><img src='" + title(spiderMan)"' alt="...">")*/
 });
 
-	var spiderMan = 'http://www.omdbapi.com/?t=spider-man&plot=full&apikey=3a181f1c';
-	var spiderMan2 = 'http://www.omdbapi.com/?t=spider-man-2&apikey=3a181f1c';
-	var spiderMan3 = 'http://www.omdbapi.com/?t=spider-man-3&apikey=3a181f1c';
-	var AmazingSpiderMan = 'http://www.omdbapi.com/?t=amazing-spider-man&apikey=3a181f1c';
-	var AmazingSpiderMan2 = 'http://www.omdbapi.com/?t=amazing-spider-man-2&apikey=3a181f1c';
-	var capitanAmerica = 'http://www.omdbapi.com/?t=captain+america&apikey=3a181f1c';
-	var ironMan = 'http://www.omdbapi.com/?t=iron+man&apikey=3a181f1c';
-	var ironMan2 = 'http://www.omdbapi.com/?t=iron+man+2&apikey=3a181f1c';
-	var ironMan3 = 'http://www.omdbapi.com/?t=iron+man+3&apikey=3a181f1c';
-	var Thor = 'http://www.omdbapi.com/?t=thor&apikey=3a181f1c';
 
-	show(spiderMan)
+  
 
-	function show(movie){
-		$.getJSON(movie).then(function(response){
-			console.log(response);
-		});
-		 
-		
-	}
+
