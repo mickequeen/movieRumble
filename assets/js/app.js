5ade43ee3d1d60ea34ea2acf4fbd62b85aac4813
Lore Palominos <lorena.palominos@gmail.com>
2018-01-09 21:30:53 -0300

    añadiendo botones y firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,18 +12,12 @@
 
 $(document).ready(function(){
 
-  // Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyDAkx_-jelOE2xPjZRE9dbsi6QxrVvnnsE",
-      authDomain: "movierumble-c0999.firebaseapp.com",
-      databaseURL: "https://movierumble-c0999.firebaseio.com",
-      projectId: "movierumble-c0999",
-      storageBucket: "",
-      messagingSenderId: "919196728572"
-    };
-    firebase.initializeApp(config);
+  $ ('#login').click(function(){
+    $ ('#modal').modal ();
+  });
 
-    title(spiderMan)
+
+  title(spiderMan)
 
   function title(movie){
     $.getJSON(movie).then(function(response){
@@ -35,30 +29,6 @@ $(document).ready(function(){
 });
 
 
-
-	var spiderMan = 'http://www.omdbapi.com/?t=spider-man&plot=full&apikey=3a181f1c';
-	var spiderMan2 = 'http://www.omdbapi.com/?t=spider-man-2&apikey=3a181f1c';
-	var spiderMan3 = 'http://www.omdbapi.com/?t=spider-man-3&apikey=3a181f1c';
-	var AmazingSpiderMan = 'http://www.omdbapi.com/?t=amazing-spider-man&apikey=3a181f1c';
-	var AmazingSpiderMan2 = 'http://www.omdbapi.com/?t=amazing-spider-man-2&apikey=3a181f1c';
-	var capitanAmerica = 'http://www.omdbapi.com/?t=captain+america&apikey=3a181f1c';
-	var ironMan = 'http://www.omdbapi.com/?t=iron+man&apikey=3a181f1c';
-	var ironMan2 = 'http://www.omdbapi.com/?t=iron+man+2&apikey=3a181f1c';
-	var ironMan3 = 'http://www.omdbapi.com/?t=iron+man+3&apikey=3a181f1c';
-	var Thor = 'http://www.omdbapi.com/?t=thor&apikey=3a181f1c';
-
-	show(spiderMan)
-
-	function show(movie){
-		$.getJSON(movie).then(function(response){
-			console.log(response);
-		});
-		 
-		
-	}
-
- 
-
 /*
 *funcion comentarios
 */
@@ -68,3 +38,6 @@ $("#btn").click(function comm(){
   $("#post").append("<span id='usuario'><i class='fa fa-user-circle' aria-hidden='true'></i></span>" + comment + "</br>"+"</div>" );
     
 });
+
+
+
