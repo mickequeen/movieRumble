38fbd218aeae237cf93b05ffcd56dd485d798b75
mickequeen <mickequeen@gmail.com>
2018-01-11 21:42:03 -0300

    top battles

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,9 +9,6 @@ var idHeartThree = 0;
 var idBombThree = 0;
 var idTrashThree = 0;
 
-  var AmazingSpiderMan = 'http://www.omdbapi.com/?t=amazing-spider-man&apikey=3a181f1c';
-  var AmazingSpiderMan2 = 'http://www.omdbapi.com/?t=amazing-spider-man-2&apikey=3a181f1c';
-  var capitanAmerica = 'http://www.omdbapi.com/?t=captain+america&apikey=3a181f1c';
   var ironMan = 'http://www.omdbapi.com/?t=iron+man&apikey=3a181f1c';
   var ironMan2 = 'http://www.omdbapi.com/?t=iron+man+2&apikey=3a181f1c';
   var ironMan3 = 'http://www.omdbapi.com/?t=iron+man+3&apikey=3a181f1c';
