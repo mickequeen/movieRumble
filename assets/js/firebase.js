0f282e2f2980e11acce1f1e31b9877831257cbae
Lore Palominos <lorena.palominos@gmail.com>
2018-01-11 23:18:42 -0300

    asdf

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -17,6 +17,16 @@
   *en que se encuentra el usuario (logueado o deslogueado)
   */
 
+  function observador (){
+    firebase.auth().onAuthStateChanged(function(user){
+      if(user){
+        console.log('Logueado ok');
+      } else {
+        console.log('NO estás logueado');
+      }
+    });
+  }
+
 
   /*
   *Loguear usuario
@@ -25,6 +35,7 @@
     var mail = $("#txtEmail").val();
     var pass = $("#txtPassword").val();
     var auth = firebase.auth();
+    alert('¡Welcome!');
 
     var promise= auth.signInWithEmailAndPassword(mail, pass);
     promise.catch(e=>alert(e.message));
@@ -38,7 +49,7 @@
     var mail = $("#txtSignupEmail").val();
     var pass = $("#txtSignupPassword").val();
     var auth = firebase.auth();
-
+    alert('An email has been send to your email account');
     var promise = auth.createUserWithEmailAndPassword(mail, pass);
     promise.catch(e => alert(e.message));
     
@@ -49,7 +60,7 @@
   *Para desloguearse
   */
   $('#btnLogout').click (function () {
-    alert ('¡Hasta Pronto!');
+    alert ('¡See you soon!');
     firebase.auth ().signOut ().then (function () {
       location.reload ();
     }) .catch (function (error){
@@ -65,7 +76,7 @@
 function verify () {
   var user = firebase.auth().currentUser;
   user.sendEmailVerification().then(function() {
-    alert ('Enviando email de verificación...');
+    alert ('Sendin verification email...');
   }).catch(function(error) {
     // An error happened.
   });
